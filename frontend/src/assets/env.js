af45257098228087e0b2e122074ed719286b3f3f
Brayan-22 <alejandroriveros100@gmail.com>
2026-05-08 16:45:33 -0500

    proxy

--- frontend/src/assets/env.js
+++ frontend/src/assets/env.js
@@ -1,5 +1,5 @@
 window.__env = window.__env || {};
 window.__env.BACKEND_URL = "__BACKEND_URL__";
-if (window.__env.BACKEND_URL === "__BACKEND_URL__") {
+if (!window.__env.BACKEND_URL || window.__env.BACKEND_URL.startsWith("__")) {
 	window.__env.BACKEND_URL = "/api";
 }
